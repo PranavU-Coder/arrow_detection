83b52349e1a057ee98ce35e9341c2a8b4d339375
PranavU-Coder <pranavu8406@gmail.com>
2025-06-30 14:41:43 +0530

    first try {only left}

---
+++ capture.py
@@ -0,0 +1,27 @@
+import cv2
+from ultralytics import YOLO
+
+model = YOLO("runs/detect/train/weights/best.pt")
+
+cap = cv2.VideoCapture(0)
+
+cap.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
+cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
+
+while True:
+    ret, frame = cap.read()
+    
+    if not ret:
+        break
+    
+    results = model(frame,conf=0.75)
+    
+    annotated_frame = results[0].plot()
+    
+    cv2.imshow('Arrow Detection', annotated_frame)
+    
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
